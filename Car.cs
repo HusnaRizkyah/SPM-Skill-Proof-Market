9c80215eabe1f558ad9a9393ce8b1efb23cc166d
Husna Rizkyah <husnarizkyah@gmail.com>
2025-11-28 14:01:40 +0700

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car : MonoBehaviour
+{
+
+    int usia;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void main(){
+        int umur;
+    }
+}
+class motor{
+    
+}
